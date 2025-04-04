c664842f4a144bb6169e357532c2bd053a340448
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2025-04-03 22:09:43 -0300

    fix: fine adjust in the lane start

--- src/components/days-header.jsx
+++ src/components/days-header.jsx
@@ -5,7 +5,7 @@ const DaysHeader = ({ days }) => {
       style={{ gridTemplateColumns: `repeat(${days.length}, 1fr)` }}
     >
       {days.map((day, key) => (
-        <div key={key} className="flex flex-col gap-4 items-center justify-center">
+        <div key={key} className="flex flex-col gap-4  justify-center">
           <span className="text-xs text-gray-400">{day + 1}</span>
           <div className="h-[calc(100vh-20rem)] w-1 absolute top-52 mt-1 border-r-2 border-dashed border-blue-100"></div>
         </div>
